3b4bfe426bc2d591012144172aa9a474a861c092
Ustintys <106092157+Ustintys@users.noreply.github.com>
2022-05-23 21:57:58 +0300

    Update xml.js

--- xml.js
+++ xml.js
@@ -1,5 +1,5 @@
 let xml = new XMLHttpRequest();
-xml.open("GET", "https://github.com/Ustintys/Phototrip/blob/main/XML.xml", false);
+xml.open("GET", "https://raw.githubusercontent.com/Ustintys/Ustintys.github.io/master/XML.xml", false);
 xml.send();
 let datas = xml.responseXML.querySelector("information");
 Array.from(document.getElementsByClassName("middle-info-name"))[0].innerHTML = xml.responseXML.querySelector("information").children[0].innerHTML;
